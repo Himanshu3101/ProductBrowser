301a1333c76fc026d4682cfd6169f6aeb27800f7
Himanshu3101 <himanshusrivastava3101@gmail.com>
2026-07-13 18:35:38 +0530

    Initial commit for setup

---
+++ shared/src/commonMain/kotlin/org/himanshu/productbrowser/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package org.himanshu.productbrowser
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/org/himanshu/productbrowser/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.himanshu.productbrowser
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
