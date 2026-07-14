0c37de6c39c5c877ccd9ae869aac075551c552c1
Himanshu3101 <himanshusrivastava3101@gmail.com>
2026-07-14 17:11:25 +0530

    feat: complete product browser app with search, navigation and unit tests

--- shared/src/commonMain/kotlin/org/himanshu/productbrowser/App.kt
+++ shared/src/commonMain/kotlin/org/himanshu/productbrowser/App.kt
@@ -3,7 +3,6 @@ package org.himanshu.productbrowser
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import org.himanshu.productbrowser.di.AppModule
 import org.himanshu.productbrowser.presentation.navigation.AppNavigation
 
 @Composable
